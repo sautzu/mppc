8906a6d9ec7f0ba091bde6dc9b954e5f7989e74d
suzuki <asuzuki@hep.phys.titech.ac.jp>
2020-09-29 13:46:57 +0900

    WIP: MPPCの整理

---
+++ include/VolumeMPPC.hh
@@ -0,0 +1,61 @@
+/**
+ * MPPC Logical Volume
+ **/
+
+#include "G4PVPlacement.hh"
+#include "G4Box.hh"
+#include "G4LogicalVolume.hh"
+#include "G4VPhysicalVolume.hh"
+#include "G4Material.hh"
+
+class VolumeMPPC
+{
+public:
+    VolumeMPPC();
+    //LogicalVolumeを返す
+    G4LogicalVolume *getLogicalVolume();
+    //可視化
+    void VisAttributes();
+    //境界の反射
+    void SurfaceProperties();
+    //測定部分のLogicalVolumeを返す
+    G4LogicalVolume *getMPPCLogical();
+
+private:
+    //Solid
+    G4Box *fLocal_box;
+    G4Box *fMPPC_box;
+    G4Box *fWin_box;
+    G4Box *fEnve_box;
+
+    //LogicalVolume
+    G4LogicalVolume *fLocal_log;
+    G4LogicalVolume *fMPPC_log;
+    G4LogicalVolume *fWin_log;
+    G4LogicalVolume *fEnve_log;
+
+    //PhysicalVolume
+    G4PVPlacement *fMPPC_phy;
+    G4PVPlacement *fWin_phy;
+    G4PVPlacement *fEnve_phy;
+
+    //LocalWorld
+    G4double local_height;
+    G4double local_width;
+
+    //MPPC
+    G4double mppc_height;
+    G4double mppc_width;
+    // window
+    G4double win_longside;
+    G4double win_shortside;
+    G4double win_width;
+    //Envelop
+    G4double enve_longside;
+    G4double enve_shortside;
+    G4double enve_width;
+
+    G4VisAttributes *mppc_va;
+    G4VisAttributes *win_va;
+    G4VisAttributes *enve_va;
+};
---
+++ src/VolumeMPPC.cc
@@ -0,0 +1,97 @@
+#include "VolumeMPPC.hh"
+#include "G4VisAttributes.hh"
+#include "G4MaterialPropertiesTable.hh"
+#include "G4OpticalSurface.hh"
+#include "G4LogicalBorderSurface.hh"
+#include "G4LogicalSkinSurface.hh"
+#include "G4SystemOfUnits.hh"
+
+VolumeMPPC::VolumeMPPC()
+{
+    local_height = 6.0 * mm;
+    local_width = 2.1 * mm;
+
+    mppc_height = 3.0 * mm;
+    mppc_width = 0.05 * mm;
+
+    win_longside = 6.0 * mm;
+    win_shortside = 6.0 * mm;
+    win_width = 0.1 * mm;
+
+    enve_longside = 6.0 * mm;
+    enve_shortside = 6.0 * mm;
+    enve_width = 2.0 * mm;
+
+    //形の定義
+    fLocal_box = new G4Box("mppc_local", local_height / 2., local_height / 2., local_width / 2.);
+    fMPPC_box = new G4Box("mppc_box", mppc_height / 2., mppc_height / 2., mppc_width / 2.);
+    fWin_box = new G4Box("Win_box", win_longside / 2., win_shortside / 2., win_width / 2.);
+    fEnve_box = new G4Box("Enve_box", enve_longside / 2., enve_shortside / 2., enve_width / 2.);
+
+    //物質の定義
+    fLocal_log = new G4LogicalVolume(fLocal_box, G4Material::GetMaterial("Air"), "Air", 0, 0, 0);
+    fMPPC_log = new G4LogicalVolume(fMPPC_box, G4Material::GetMaterial("Al"), "mppc_log", 0, 0, 0);
+    fWin_log = new G4LogicalVolume(fWin_box, G4Material::GetMaterial("Silicone"), "win_log", 0, 0, 0);
+    fEnve_log = new G4LogicalVolume(fEnve_box, G4Material::GetMaterial("ABS"), "enve_log", 0, 0, 0);
+
+    //MPPC全体の定義
+    G4ThreeVector MPPC_posi(0, 0, 1.025 * mm);
+    G4ThreeVector win_posi(0, 0, 1.0 * mm);
+    G4ThreeVector Enve_posi(0, 0, -0.05 * mm);
+
+    fWin_phy = new G4PVPlacement(0, win_posi, fWin_log, "Window", fLocal_log, false, 0);
+    fEnve_phy = new G4PVPlacement(0, Enve_posi, fEnve_log, "Enve", fLocal_log, false, 0);
+    fMPPC_phy = new G4PVPlacement(0, MPPC_posi, fMPPC_log, "MPPC", fLocal_log, false, 0);
+}
+
+G4LogicalVolume *VolumeMPPC::getLogicalVolume()
+{
+    return fLocal_log;
+}
+
+G4LogicalVolume *VolumeMPPC::getMPPCLogical()
+{
+    return fMPPC_log;
+}
+
+void VolumeMPPC::VisAttributes()
+{
+    mppc_va = new G4VisAttributes(G4Colour(0.2, 0.4, 0.2));
+    fMPPC_log->SetVisAttributes(mppc_va);
+    win_va = new G4VisAttributes(G4Colour(0.4, 0.4, 0.6));
+    fWin_log->SetVisAttributes(win_va);
+    enve_va = new G4VisAttributes(G4Colour(0.8, 0.8, 0.8));
+    fEnve_log->SetVisAttributes(enve_va);
+}
+
+void VolumeMPPC::SurfaceProperties()
+{
+    G4double ephoton[] = {2.76 * eV, 3.06 * eV};
+    const G4int num = sizeof(ephoton) / sizeof(G4double);
+
+    //Windowの反射設定 (Envelope周り)
+    G4double Window_RIND[] = {1.0, 1.0};
+    G4double Window_REF[] = {1.0, 1.0};
+    G4MaterialPropertiesTable *Window_PT = new G4MaterialPropertiesTable();
+    Window_PT->AddProperty("RINDEX", ephoton, Window_RIND, num);
+    Window_PT->AddProperty("REFLECTIVITY", ephoton, Window_REF, num);
+    G4OpticalSurface *Window_Surface = new G4OpticalSurface("WindowSurface", glisur, groundbackpainted, dielectric_dielectric);
+    Window_Surface->SetMaterialPropertiesTable(Window_PT);
+
+    G4LogicalBorderSurface *surface1 = new G4LogicalBorderSurface("Window_surf", fWin_phy, fEnve_phy, Window_Surface);
+    //TODO:windowとAirの境界
+
+    //mppcの反射設定(光子感度)
+    G4double MPPC_ReR[] = {1.92, 1.92};
+    G4double MPPC_ImR[] = {1.69, 1.69};
+    G4double MPPC_EFF[] = {1.0, 1.0}; //enables 'detection' of photons
+    G4MaterialPropertiesTable *MPPC_mt = new G4MaterialPropertiesTable();
+    MPPC_mt->AddProperty("REALRINDEX", ephoton, MPPC_ReR, num);
+    MPPC_mt->AddProperty("IMAGINARYRINDEX", ephoton, MPPC_ImR, num);
+    MPPC_mt->AddProperty("EFFICIENCY", ephoton, MPPC_EFF, num);
+    G4OpticalSurface *MPPC_Surface = new G4OpticalSurface("MPPCSurface", glisur, polished, dielectric_metal);
+    MPPC_Surface->SetMaterialPropertiesTable(MPPC_mt);
+
+    //Create logical skin surfaces
+    G4LogicalSkinSurface *mppc_surf = new G4LogicalSkinSurface("mppc_surf", fMPPC_log, MPPC_Surface);
+}
